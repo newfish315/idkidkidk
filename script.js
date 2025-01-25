c20a65a58b1a6c083bd9360367b5819b4138ca29
newfish315 <main180360@gmail.com>
2025-01-24 23:48:51 -0700

    js test

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+
+console.log('2');
+var number = 123;
+var string = 'ABC'
+console.log(string);
+
+*/
+
+function popup(){alert('woo');}
+
+document.getElementById('btn').addEventListener('click', popup);
